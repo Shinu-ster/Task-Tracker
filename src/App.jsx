808ed227bef762006d19e6bb0e2971dea1684882
Shinu-ster <sajatbazz@gmail.com>
2025-12-26 18:43:46 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <div className="text-3xl bg-[#F5F2F2] ">hello world</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='text-3xl text-center bg-[#5A7ACD] p-4 '>Task Tracker</div>
+  )
+}
+
+export default Navbar
